cc4bd988b39fe6142ffb7dc4d7c6cb3c31a079e8
super.sun <121212>
2017-12-08 15:08:52 +0800

    更新

--- microservice-consumer-movie/src/main/java/springcloud/microserviceconsumermovie/controller/MovieController.java
+++ microservice-consumer-movie/src/main/java/springcloud/microserviceconsumermovie/controller/MovieController.java
@@ -1,11 +1,16 @@
 package springcloud.microserviceconsumermovie.controller;
 
+import jdk.internal.dynalink.linker.LinkerServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import springcloud.microserviceconsumermovie.entity.User;
+
+import java.util.List;
 
 /**
  * Created by Super.Sun on 2017/12/7.
@@ -16,8 +21,16 @@ public class MovieController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/user/{id}")
     public User findById(@PathVariable Long id) {
         return this.restTemplate.getForObject("http://localhost:8000/" + id, User.class);
+    }
+
+    @GetMapping("/user/instance")
+    public List<ServiceInstance> showInfo(){
+        return this.discoveryClient.getInstances("microservice-provider-user");
     }
 }
--- microservice-provider-user/src/main/java/springcloud/microservicesimpleprovideruser/controller/UserController.java
+++ microservice-provider-user/src/main/java/springcloud/microservicesimpleprovideruser/controller/UserController.java
@@ -1,11 +1,15 @@
 package springcloud.microservicesimpleprovideruser.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import springcloud.microservicesimpleprovideruser.dao.UserRepository;
 import springcloud.microservicesimpleprovideruser.entity.User;
+
+import java.util.List;
 
 
 @RestController
@@ -13,9 +17,17 @@ public class UserController {
   @Autowired
   private UserRepository userRepository;
 
+  @Autowired
+  private DiscoveryClient discoveryClient;
+
   @GetMapping("/{id}")
   public User findById(@PathVariable Long id) {
     User findOne = this.userRepository.findOne(id);
     return findOne;
+  }
+
+  @GetMapping("/user/instance")
+  public List<ServiceInstance> showInfo(){
+    return this.discoveryClient.getInstances("microservice-provider-user");
   }
 }
