13f7ee43cf5fe107696d86855d405bbab0985062
super.sun <121212>
2018-02-27 14:57:52 +0800

    更新

--- microservice-gateway-zuul/src/main/java/com/gatewat/zuul/ZuulApplication.java
+++ microservice-gateway-zuul/src/main/java/com/gatewat/zuul/ZuulApplication.java
@@ -1,12 +1,20 @@
 package com.gatewat.zuul;
 
+import com.gatewat.zuul.filter.pre.PreRequestLogFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
 public class ZuulApplication {
+
+  @Bean
+  public PreRequestLogFilter preRequestLogFilter() {
+    return new PreRequestLogFilter();
+  }
+
   public static void main(String[] args) {
     SpringApplication.run(ZuulApplication.class, args);
   }
