a6f9fd60ca87249b9b4007918fd5b1462872ee39
super.sun <121212>
2018-03-05 09:36:50 +0800

    更新

---
+++ microservice-config-client/src/main/java/com/coinfig/client/ConfigClientApplication.java
@@ -0,0 +1,15 @@
+package com.coinfig.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Super.Sun on 2018/2/27.
+ */
+
+@SpringBootApplication
+public class ConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+}
---
+++ microservice-config-server/src/main/java/com/config/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by Super.Sun on 2018/2/27.
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
