1e09e7b6d0d0a8fdbab608e8d390977a964b77b6
super.sun <121212>
2018-03-05 14:06:48 +0800

    更新

---
+++ microservice-trace-zipkin-server/src/main/java/com/zipkin/ZipkinServerApplication.java
@@ -0,0 +1,17 @@
+package com.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
+
+/**
+ * Created by Super.Sun on 2018/3/5.
+ */
+@SpringBootApplication
+@EnableZipkinServer
+public class ZipkinServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServerApplication.class, args);
+    }
+}
